fdb1c6c369885be30e4eaf4a10992588f5ff1c7e
Chandon Porter <cneilporter94@gmail.com>
2019-09-03 15:35:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU SUNNY!"
+    return "I LOVE YOU SUNNY!"
   else
 
 # Whatever you say to grandma, she should respond with
